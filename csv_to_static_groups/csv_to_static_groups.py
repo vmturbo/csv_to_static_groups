83320b7e5580116012231221e3cc431c0adc382a
Austin Portal <austinportal@users.noreply.github.com>
2018-05-14 15:46:56 -0400

    Prepare for release 'v1.1.0'

--- csv_to_static_groups/csv_to_static_groups.py
+++ csv_to_static_groups/csv_to_static_groups.py
@@ -12,7 +12,7 @@ import sys
 import json
 from functools import wraps
 
-__version__ = "1.1.0.dev"
+__version__ = "1.1.0"
 
 ## ----------------------------------------------------
 ##   Global Variables
